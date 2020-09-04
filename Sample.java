0f30b17014600d7f188e0558841eee4dc0c9b4b4
mahimajyothi <mahimajyothi1@gmail.com>
2020-09-04 15:04:44 +0530

    added program

---
+++ Sample.java
@@ -0,0 +1,15 @@
+public class Sample{
+
+       public static void main(String[] args){
+	   
+	        
+			
+			   System.out.println("Hello World... how are you");
+			   
+	        
+	    
+		}
+	
+	}
+        			  
+	        
